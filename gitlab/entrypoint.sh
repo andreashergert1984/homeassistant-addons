92e1df16e3b6655bd5d6e775283d77e02789bc73
Andreas Hergert <andreas.hergert@otrs.com>
2024-03-22 21:42:05 +0100

    fix

--- gitlab/entrypoint.sh
+++ gitlab/entrypoint.sh
@@ -4,18 +4,34 @@
 GITLAB_EXTERNAL_URL="http://$(hostname -i)"
 echo $GITLAB_EXTERNAL_URL
 echo "starting current version"
-echo "external_url '${GITLAB_EXTERNAL_URL}'" >> /etc/gitlab/gitlab.rb
-
-# Configure trusted proxies and other settings as needed
-echo "nginx['listen_port'] = 80;" >> /etc/gitlab/gitlab.rb
-echo "nginx['listen_https'] = false;" >> /etc/gitlab/gitlab.rb
-echo "gitlab_rails['trusted_proxies'] = ['172.30.32.2']" >> /etc/gitlab/gitlab.rb
-echo "gitlab_rails['initial_root_password'] = '<my_strong_password>'" >> /etc/gitlab/gitlab.rb
 
 mkdir -p /config/gitlab
-mkdir -p /data/gitlab
+FILE=/config/gitlab/gitlab.rb
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist."
+    cp /opt/gitlab/etc/gitlab.rb.template /etc/gitlab.rb
 
-echo 'git_data_dirs({ "default" => { "path" => "/data/gitlab/git-data" } })' >> /etc/gitlab/gitlab.rb
+    rm /etc/gitlab/gitlab.rb
+
+    ln -s /config/gitlab/gitlab.rb /etc/gitlab/gitlab.rb
+
+    echo "external_url '${GITLAB_EXTERNAL_URL}'" >> /etc/gitlab/gitlab.rb
+    echo "nginx['listen_port'] = 80;" >> /etc/gitlab/gitlab.rb
+    echo "nginx['listen_https'] = false;" >> /etc/gitlab/gitlab.rb
+    echo "gitlab_rails['trusted_proxies'] = ['172.30.32.2']" >> /etc/gitlab/gitlab.rb
+    echo "gitlab_rails['initial_root_password'] = '<my_strong_password>'" >> /etc/gitlab/gitlab.rb
+
+fi
+
+rm /etc/gitlab/gitlab.rb
+
+ln -s /config/gitlab/gitlab.rb /etc/gitlab/gitlab.rb
+
+#mkdir -p /data/gitlab
+
+#echo 'git_data_dirs({ "default" => { "path" => "/data/gitlab/git-data" } })' >> /etc/gitlab/gitlab.rb
 
 # Reconfigure GitLab to apply changes
 #gitlab-ctl reconfigure
